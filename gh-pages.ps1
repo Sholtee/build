2d0c28db4b560dd3ff5ba5b7d110acbfdeacd3ec
Dénes Solti <sodnaatx@gmail.com>
2019-11-18 11:14:39 +0100

    gh-pages.ps1 fix [remove obsolete files]

--- gh-pages.ps1
+++ gh-pages.ps1
@@ -4,6 +4,9 @@
 # Author: Denes Solti
 #
 function GH-Pages() {
+  $DOC_FOLDER="doc"
+  $PERF_FOLDER="perf"
+
   Write-Host Preparing DOCS repo...
 
   $repodir=Path-Combine (Directory-Path $PROJECT.solution | Resolve-Path), $PROJECT.docsbranch
@@ -23,22 +26,19 @@ function GH-Pages() {
 
     if ($updateAPI) {
       Write-Host Building API docs...
-      BuildNMove -projectDir (Directory-Path $PROJECT.app) -docsOutput (Path-Combine $PROJECT.artifacts, "doc")
+      BuildNMove -projectDir (Directory-Path $PROJECT.app) -docsOutput (Path-Combine $PROJECT.artifacts, $DOC_FOLDER)
     } else {
       Write-Host Building benchmark docs...
-      BuildNMove -projectDir (Directory-Path $PROJECT.perftests) -docsOutput (Path-Combine $PROJECT.artifacts, "BenchmarkDotNet.Artifacts", "perf")
+      BuildNMove -projectDir (Directory-Path $PROJECT.perftests) -docsOutput (Path-Combine $PROJECT.artifacts, "BenchmarkDotNet.Artifacts", $PERF_FOLDER)
     }
 
-    function Commit([string] $message) {
+    function Commit([Parameter(Position = 0)][string]$path, [string] $message) {
       Write-Host Committing changes...
       $oldLocation=Get-Location
       Set-Location -path $repodir
       try {
-        #Adding folders would freeze git.exe =(
-        Get-ChildItem -path "." -Recurse | where { !$_.PSIsContainer } | foreach {
-          Exec "git.exe" -commandArgs "add `"$($_.FullName)`"" -ignoreError
-        }       
-        Exec "git.exe" -commandArgs "commit -m `"$($message)`"" -ignoreError
+        Exec "git.exe" -commandArgs "add `"$($path)`"" -ignoreError -noLog    
+        Exec "git.exe" -commandArgs "commit -m `"$($message)`"" -ignoreError -noLog
         Exec "git.exe" -commandArgs "push origin $($PROJECT.docsbranch)"
       } finally {
         Set-Location -path $oldLocation	  
@@ -46,9 +46,9 @@ function GH-Pages() {
     }
 
     if ($updateAPI) {
-      Commit -message "docs up"
+      Commit $DOC_FOLDER -message "docs up"
     } else {
-      Commit -message "benchmarks up"	
+      Commit $PERF_FOLDER -message "benchmarks up"	
     }	
   } finally {
     Remove-Directory $repodir
