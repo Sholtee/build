3a7b72d8ebd59a71177418f5ac9bfde759e8672a
Dénes Solti <sodnaatx@gmail.com>
2020-05-12 14:46:38 +0200

    generate NuGet package(s) from .sln instead of .csproj

--- pack.ps1
+++ pack.ps1
@@ -7,7 +7,7 @@ function Pack() {
   Remove-Directory $PROJECT.bin
 
   $currentbranch=Exec "git.exe" -commandArgs "rev-parse --abbrev-ref HEAD" -redirectOutput
-  Exec "dotnet.exe" -commandArgs "pack `"$($PROJECT.app | Resolve-Path)`" -c Release /p:CurrentBranch=$($currentbranch)"
+  Exec "dotnet.exe" -commandArgs "pack `"$($PROJECT.solution | Resolve-Path)`" -c Release /p:CurrentBranch=$($currentbranch)"
   
-  return (Get-ChildItem -path (Path-Combine ($PROJECT.bin | Resolve-Path), "*.nupkg")).FullName
+  return Get-ChildItem -path (Path-Combine ($PROJECT.bin | Resolve-Path), "*.nupkg")
 }
