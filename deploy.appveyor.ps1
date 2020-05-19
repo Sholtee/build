a6cdac10e4d740c3d4bfd08f41be491cbe98e573
Dénes Solti <sodnaatx@gmail.com>
2020-05-19 10:16:15 +0200

    deploy.appveyor.ps1 fix [wrong path]

--- deploy.appveyor.ps1
+++ deploy.appveyor.ps1
@@ -7,9 +7,9 @@ function Deploy() {
   if ($Env:APPVEYOR_REPO_TAG_NAME -match "^v\d+.\d+.\d+[-\w]*$") {
     Write-Host Deploying...
 	
-	Pack | foreach {
+    Pack | foreach {
       Write-Host "Deploying $($_.Name)"
-      Exec "dotnet.exe" -commandArgs "nuget push `"$_.FullName`" -s https://api.nuget.org/v3/index.json -k $($Env:NUGET_REPO_TOKEN)"
+      Exec "dotnet.exe" -commandArgs "nuget push `"$_`" -s https://api.nuget.org/v3/index.json -k $($Env:NUGET_REPO_TOKEN)"
     }
   }
 }
