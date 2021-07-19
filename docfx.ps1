42c27b5d4dd6001c81c569c92a7819715f615241
Dénes Solti <sodnaatx@gmail.com>
2021-07-19 19:48:05 +0200

    docfx.ps1: DocFX up

--- docfx.ps1
+++ docfx.ps1
@@ -4,6 +4,6 @@
 # Author: Denes Solti
 #
 function DocFx([Parameter(Position = 0)][string] $jsonpath) {
-  $docFx=Path-Combine (Get-Package "docfx.console" -Version "2.46.0"), "tools", "docfx.exe" | Resolve-Path
+  $docFx=Path-Combine (Get-Package "docfx.console" -Version "2.58.0"), "tools", "docfx.exe" | Resolve-Path
   Exec $docFx -commandArgs $jsonpath
 }
