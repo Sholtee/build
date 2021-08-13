09fbdd0fa6244f25a84caa9119fa10522603a939
Dénes Solti <sodnaatx@gmail.com>
2021-08-13 08:57:46 +0200

    push.appveyor.ps1: try to migrate to coveralls.net VI

--- push.appveyor.ps1
+++ push.appveyor.ps1
@@ -10,7 +10,7 @@ function Push-Test-Results() {
 
   Get-ChildItem -Path (Path-Combine $PROJECT.artifacts, "*.xml") -Exclude "opencover.xml" | foreach {
     Write-Host "Uploading test result: $($_.Name)"
-    $client.UploadFile("https://ci.appveyor.com/api/testresults/nunit/$($Env:APPVEYOR_JOB_ID)", $_.ToString())
+    $client.UploadFile("https://ci.appveyor.com/api/testresults/nunit/$($Env:APPVEYOR_JOB_ID)", $_.FullName)
   }
 
   if ($PROJECT.web -is [string]) {
@@ -34,7 +34,7 @@ function Push-CoverageReports([Parameter(Position = 0)][string[]] $reports) {
 
   Get-ChildItem -Path (Path-Combine $PROJECT.artifacts, "*") -Include $reports | foreach {
     Write-Host "Uploading coverage report: $($_.Name)"
-    Exec $coveralls -commandArgs "--$([System.IO.Path]::GetFileNameWithoutExtension($_.ToString())) -i $($_) --repoToken $($Env:COVERALLS_REPO_TOKEN) --useRelativePaths --commitId $($Env:APPVEYOR_REPO_COMMIT) --commitBranch $($Env:APPVEYOR_REPO_BRANCH) --commitAuthor $($Env:APPVEYOR_REPO_COMMIT_AUTHOR) --commitEmail $($Env:APPVEYOR_REPO_COMMIT_AUTHOR_EMAIL) --commitMessage $($Env:APPVEYOR_REPO_COMMIT_MESSAGE) --jobId $($Env:APPVEYOR_JOB_ID) --serviceName appveyor --serviceNumber $($Env:APPVEYOR_BUILD_NUMBER) --parallel" -cwd (Resolve-Path "..")
+    Exec $coveralls -commandArgs "--$([System.IO.Path]::GetFileNameWithoutExtension($_.FullName)) -i `"$($_.FullName)`" --repoToken $($Env:COVERALLS_REPO_TOKEN) --commitId $($Env:APPVEYOR_REPO_COMMIT) --commitBranch $($Env:APPVEYOR_REPO_BRANCH) --commitAuthor `"$($Env:APPVEYOR_REPO_COMMIT_AUTHOR)`" --commitEmail $($Env:APPVEYOR_REPO_COMMIT_AUTHOR_EMAIL) --commitMessage `"$($Env:APPVEYOR_REPO_COMMIT_MESSAGE)`" --jobId $($Env:APPVEYOR_JOB_ID) --serviceName appveyor --serviceNumber $($Env:APPVEYOR_BUILD_NUMBER) --parallel" -cwd (Resolve-Path "..")
   }
 
   Exec $coveralls -commandArgs "--completeParallelWork --repoToken $($Env:COVERALLS_REPO_TOKEN) --serviceNumber $($Env:APPVEYOR_BUILD_NUMBER)" -cwd (Resolve-Path "..")
