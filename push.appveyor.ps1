0d279cfcc33c18d583a357ae3886fdebc6b0a54f
Dénes Solti <sodnaatx@gmail.com>
2021-08-13 06:19:47 +0200

    push.appveyor.ps1: try to migrate to coveralls.net I

--- push.appveyor.ps1
+++ push.appveyor.ps1
@@ -7,32 +7,18 @@ function Push-Test-Results() {
   if ($Env:APPVEYOR_REPO_TAG_NAME -is [string] -and $Env:APPVEYOR_REPO_TAG_NAME.StartsWith("perf")) { return }
 
   $client= New-Object System.Net.WebClient
-  $coveragexml="coverage.xml"
-	
-  Get-ChildItem -Path (Path-Combine $PROJECT.artifacts, "*.xml") -Exclude $coveragexml | foreach {
+
+  Get-ChildItem -Path (Path-Combine $PROJECT.artifacts, "*.xml") -Exclude "coverage.xml" | foreach {
     Write-Host "Uploading test result: $($_.Name)"
     $client.UploadFile("https://ci.appveyor.com/api/testresults/nunit/$($Env:APPVEYOR_JOB_ID)", $_.ToString())
-  }
-
-  $coveragereport=Path-Combine $PROJECT.artifacts, $coveragexml
- 
-  if (Test-Path $coveragereport) {
-    Write-Host "Uploading coverage report..."
-
-    $coveralls=Path-Combine (Get-Package "coveralls.io" -Version "1.4.2"), "tools", "coveralls.net.exe" | Resolve-Path
-
-    $commandArgs="--opencover `"$($coveragereport)`" -r $($Env:COVERALLS_REPO_TOKEN)"
-    if ($Env:DEBUG_CI) { $commandArgs+=" -d" }
-
-    Exec $coveralls -commandArgs $commandArgs -cwd (Resolve-Path "..")
   }
 
   if ($PROJECT.web -is [string]) {
     Write-Host "Uploading WEB test results..."
     Web-PushResults
-    Write-Host "Uploading WEB coverage report..."
-    Web-PushCoverage
   }
+
+  Push-CoverageReports "coverage.xml", "lcov.info"
 }
 
 function Push-Artifact([Parameter(Position = 0)][string]$pattern) {
@@ -41,4 +27,19 @@ function Push-Artifact([Parameter(Position = 0)][string]$pattern) {
   if (!(Directory-Path $pattern | Test-Path)) { return }
   
   Get-ChildItem -path $pattern | foreach { Push-AppveyorArtifact $_.FullName }
+}
+
+function Push-CoverageReports([Parameter(Position = 0)][string[]] $reports) {
+  $coveralls=Path-Combine (Get-Package "coveralls.net" -Version "3.0.0" -IsTool), "csmacnz.Coveralls.exe" | Resolve-Path
+
+  $i=$reports.Length
+  Get-ChildItem -Path $PROJECT.artifacts -Include $reports | foreach {
+    $last=$i-- -EQ 0
+    Write-Host "Uploading coverage report: $($_.Name) [last: $($last)]"
+
+    $commandArgs="--opencover -i $($_) --repoToken $($Env:COVERALLS_REPO_TOKEN) --useRelativePaths --commitId $($Env:APPVEYOR_REPO_COMMIT) --commitBranch $($Env:APPVEYOR_REPO_BRANCH) --commitAuthor $($Env:APPVEYOR_REPO_COMMIT_AUTHOR) --commitEmail $($Env:APPVEYOR_REPO_COMMIT_AUTHOR_EMAIL) --commitMessage $($Env:APPVEYOR_REPO_COMMIT_MESSAGE) --jobId $($Env:APPVEYOR_BUILD_NUMBER) --serviceName appveyor --parallel"
+    if ($last) { $commandArgs+= "--completeParallelWork"}
+
+    Exec $coveralls -commandArgs  -cwd (Resolve-Path "..")
+  }
 }
