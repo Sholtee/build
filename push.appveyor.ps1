ea1bdbdb518a1fa07b4bfca638f67a08f7599383
Dénes Solti <sodnaatx@gmail.com>
2021-08-17 14:46:26 +0200

    push.appveyor.ps1: debug support

--- push.appveyor.ps1
+++ push.appveyor.ps1
@@ -34,7 +34,13 @@ function Push-CoverageReports([Parameter(Position = 0)][string[]] $reports) {
 
   Get-ChildItem -Path (Path-Combine $PROJECT.artifacts, "*") -Include $reports | foreach {
     Write-Host "Uploading coverage report: $($_.Name)"
-    Exec $coveralls -commandArgs "--$([System.IO.Path]::GetFileNameWithoutExtension($_.FullName)) -i `"$($_.FullName)`" --repoToken $($Env:COVERALLS_REPO_TOKEN) --commitId $($Env:APPVEYOR_REPO_COMMIT) --commitBranch $($Env:APPVEYOR_REPO_BRANCH) --commitAuthor `"$($Env:APPVEYOR_REPO_COMMIT_AUTHOR)`" --commitEmail $($Env:APPVEYOR_REPO_COMMIT_AUTHOR_EMAIL) --commitMessage `"$($Env:APPVEYOR_REPO_COMMIT_MESSAGE)`" --jobId $($Env:APPVEYOR_JOB_ID) --serviceName appveyor --serviceNumber $($Env:APPVEYOR_BUILD_NUMBER) --parallel --useRelativePaths" -cwd (Resolve-Path "..")
+
+    $type=[System.IO.Path]::GetFileNameWithoutExtension($_.Name)
+
+    $commandArgs="--$($type) -i `"$($_.FullName)`" --repoToken $($Env:COVERALLS_REPO_TOKEN) --commitId $($Env:APPVEYOR_REPO_COMMIT) --commitBranch $($Env:APPVEYOR_REPO_BRANCH) --commitAuthor `"$($Env:APPVEYOR_REPO_COMMIT_AUTHOR)`" --commitEmail $($Env:APPVEYOR_REPO_COMMIT_AUTHOR_EMAIL) --commitMessage `"$($Env:APPVEYOR_REPO_COMMIT_MESSAGE)`" --jobId $($Env:APPVEYOR_JOB_ID) --serviceName appveyor --serviceNumber $($Env:APPVEYOR_BUILD_NUMBER) --parallel --useRelativePaths"
+    if ($Env:DEBUG_CI) { $commandArgs+=" -o `"$(Path-Combine $PROJECT.artifacts, $type+'.debug.json')`""}
+
+    Exec $coveralls -commandArgs $commandArgs -cwd (Resolve-Path "..")
   }
 
   Exec $coveralls -commandArgs "--completeParallelWork --repoToken $($Env:COVERALLS_REPO_TOKEN) --serviceNumber $($Env:APPVEYOR_BUILD_NUMBER)" -cwd (Resolve-Path "..")
