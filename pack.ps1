526a96af608e0f741d2bab6f7c6ed2985ec38d8c
Dénes Solti <sodnaatx@gmail.com>
2022-08-24 08:46:13 +0200

    pack.ps1: also generate symbol packages

--- pack.ps1
+++ pack.ps1
@@ -6,8 +6,11 @@
 function Pack() {
   Remove-Directory $PROJECT.bin
 
-  $currentbranch=Exec "git.exe" -commandArgs "rev-parse --abbrev-ref HEAD" -redirectOutput
-  Exec "dotnet.exe" -commandArgs "pack `"$($PROJECT.solution | Resolve-Path)`" -c Release /p:CurrentBranch=$($currentbranch)"
-  
-  return Get-ChildItem -path (Path-Combine ($PROJECT.bin | Resolve-Path), "*.nupkg") -recurse
+  $currentbranch=Exec "git.exe" -commandArgs "rev-parse --abbrev-ref HEAD" -redirectOutput 
+  $cmdArgs="pack `"$($PROJECT.solution | Resolve-Path)`" -c Release -p:CurrentBranch=$($currentbranch)"
+
+  Exec "dotnet.exe" -commandArgs $cmdArgs
+  Exec "dotnet.exe" -commandArgs "$($cmdArgs) -p:IncludeSymbols=true -p:SymbolPackageFormat=snupkg"
+
+  return Get-ChildItem -Path (Path-Combine ($PROJECT.bin | Resolve-Path), "*") -Include "*.nupkg", "*.snupkg" -Recurse
 }
