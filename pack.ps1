26438e40b1ea7536d7a4d361e1c72797c91814db
Dénes Solti <sodnaatx@gmail.com>
2022-08-24 10:09:08 +0200

    pack.ps1 fix [set repo meta properly]

--- pack.ps1
+++ pack.ps1
@@ -6,8 +6,9 @@
 function Pack() {
   Remove-Directory $PROJECT.bin
 
-  $currentbranch=Exec "git.exe" -commandArgs "rev-parse --abbrev-ref HEAD" -redirectOutput 
-  $cmdArgs="pack `"$($PROJECT.solution | Resolve-Path)`" -c Release -p:CurrentBranch=$($currentbranch)"
+  $cwd=Directory-Path ($PROJECT.solution | Resolve-Path)
+  # don't break into multiple lines. MSBUILD doesn't like it
+  $cmdArgs="pack `"$($PROJECT.solution | Resolve-Path)`" -c Release -p:RepositoryType=git -p:RepositoryCommit=$((Exec "git.exe" -cwd $cwd -commandArgs "rev-parse HEAD" -redirectOutput).Trim()) -p:RepositoryBranch=$((Exec "git.exe" -cwd $cwd -commandArgs "rev-parse --abbrev-ref HEAD" -redirectOutput).Trim()) -p:RepositoryUrl=$((Exec "git.exe" -cwd $cwd -commandArgs "config --get remote.origin.url" -redirectOutput).Trim())"
 
   Exec "dotnet.exe" -commandArgs $cmdArgs
   Exec "dotnet.exe" -commandArgs "$($cmdArgs) -p:IncludeSymbols=true -p:SymbolPackageFormat=snupkg"
