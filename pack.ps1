d7ab371021e506a941ab811d7d7ebc30f55fbc11
Dénes Solti <sodnaatx@gmail.com>
2022-08-25 06:39:26 +0200

    pack.ps1: embed commit hash into the generated assembly

--- pack.ps1
+++ pack.ps1
@@ -7,11 +7,13 @@ function Pack() {
   Remove-Directory $PROJECT.bin
 
   $cwd=Directory-Path ($PROJECT.solution | Resolve-Path)
-  # don't break into multiple lines. MSBUILD doesn't like it
-  $cmdArgs="pack `"$($PROJECT.solution | Resolve-Path)`" -c Release -p:RepositoryType=git -p:RepositoryCommit=$((Exec "git.exe" -cwd $cwd -commandArgs "rev-parse HEAD" -redirectOutput).Trim()) -p:RepositoryBranch=$((Exec "git.exe" -cwd $cwd -commandArgs "rev-parse --abbrev-ref HEAD" -redirectOutput).Trim()) -p:RepositoryUrl=$((Exec "git.exe" -cwd $cwd -commandArgs "config --get remote.origin.url" -redirectOutput).Trim())"
+  $hash=(Exec "git.exe" -cwd $cwd -commandArgs "rev-parse HEAD" -redirectOutput).Trim()
+  $branch=(Exec "git.exe" -cwd $cwd -commandArgs "rev-parse --abbrev-ref HEAD" -redirectOutput).Trim()
+  $repo=(Exec "git.exe" -cwd $cwd -commandArgs "config --get remote.origin.url" -redirectOutput).Trim()
+  $cmdArgs="pack `"$($PROJECT.solution | Resolve-Path)`" -c Release -p:RepositoryType=git -p:RepositoryCommit=$hash -p:RepositoryBranch=$branch -p:RepositoryUrl=$repo -p:CommitHash=$hash -p:CustomBeforeMicrosoftCSharpTargets=$('GitMeta.targets' | Resolve-Path)"
 
   Exec "dotnet.exe" -commandArgs $cmdArgs
-  Exec "dotnet.exe" -commandArgs "$($cmdArgs) -p:IncludeSymbols=true -p:SymbolPackageFormat=snupkg"
+  Exec "dotnet.exe" -commandArgs "$cmdArgs -p:IncludeSymbols=true -p:SymbolPackageFormat=snupkg"
 
   return Get-ChildItem -Path (Path-Combine ($PROJECT.bin | Resolve-Path), "*") -Include "*.nupkg", "*.snupkg" -Recurse
 }
