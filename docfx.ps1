4860c690cbb53baf7a9bcd76147c9b4c7d4baa4c
Dénes Solti <sodnaatx@gmail.com>
2022-08-31 06:57:26 +0200

    docfx.ps1: treat warnings as errors

--- docfx.ps1
+++ docfx.ps1
@@ -5,5 +5,5 @@
 #
 function DocFx([Parameter(Position = 0)][string] $jsonpath) {
   $docFx=Path-Combine (Get-Package "docfx.console" -Version "2.59.3"), "tools", "docfx.exe" | Resolve-Path
-  Exec $docFx -commandArgs $jsonpath
+  Exec $docFx -commandArgs "$jsonpath --warningsAsErrors"
 }
