c41a0a937dbbad25abe5241a345df9dcfd6df640
Dénes Solti <sodnaatx@gmail.com>
2022-11-20 09:35:24 +0100

    add TFM filter to test runner

--- test.ps1
+++ test.ps1
@@ -9,32 +9,34 @@ function Regular-Tests() {
 
   $opencover=Path-Combine (Get-Package "OpenCover" -Version "4.7.1221"), "tools", "OpenCover.Console.exe" | Resolve-Path
 
-  Get-ChildItem -Path (Path-Combine $PROJECT.root, $PROJECT.tests) | foreach {
+  Get-ChildItem -Path (Path-Combine $PROJECT.root, $PROJECT.tests) | ForEach-Object {
     $csproj=$_
 
     if ($PROJECT.testcommonprops -is [string]) { $propsPath=$PROJECT.testcommonprops }
     else { $propsPath=$csproj }
 
+    # Run tests against each TFMs separately to get the results not merged
     [XML]$props=Get-Content $propsPath
-    ($props.Project.PropertyGroup.TargetFrameworks | Out-String).Trim().Split(";") | foreach {
+    ($props.Project.PropertyGroup.TargetFrameworks | Out-String).Trim().Split(";") | ForEach-Object {
       $targetFw=$_
+      if (!($PROJECT.skipon -contains $targetFw)) {
+        $resultsxml="$(Path-GetFileNameWithoutExtension $csproj).$targetFw.xml"
 
-      $resultsxml="$(Path-GetFileNameWithoutExtension $csproj).$targetFw.xml"
+        $cmdArgs="
+          -target:`"$(Path-Combine $Env:ProgramFiles, 'dotnet', 'dotnet.exe')`"
+          -targetargs:`"test $csproj --configuration:Debug --framework:$targetFw --test-adapter-path:. --logger:nunit;LogFilePath=$(Path-Combine $PROJECT.artifacts, 'nunit', $resultsxml)`"
+          -output:`"$(Path-Combine $PROJECT.artifacts, 'opencover.xml')`"
+          -mergeoutput
+          -oldStyle
+          -register:user
+          -threshold:1
+          -excludebyattribute:*.ExcludeFromCoverage*
+          -filter:`"$($PROJECT.coveragefilter)`"
+          -returntargetcode
+        "
 
-      $cmdArgs="
-        -target:`"$(Path-Combine $Env:ProgramFiles, 'dotnet', 'dotnet.exe')`"
-        -targetargs:`"test $csproj --configuration:Debug --framework:$targetFw --test-adapter-path:. --logger:nunit;LogFilePath=$(Path-Combine $PROJECT.artifacts, 'nunit', $resultsxml)`"
-        -output:`"$(Path-Combine $PROJECT.artifacts, 'opencover.xml')`"
-        -mergeoutput
-        -oldStyle
-        -register:user
-        -threshold:1
-        -excludebyattribute:*.ExcludeFromCoverage*
-        -filter:`"$($PROJECT.coveragefilter)`"
-        -returntargetcode
-      "
-
-      Exec $opencover -commandArgs $cmdArgs
+        Exec $opencover -commandArgs $cmdArgs
+      }
     }
   }
 }
